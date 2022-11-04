aa99a98af688e59eb834f72e630b0cda007491ca
bizarf <snesmaster40@googlemail.com>
2022-11-04 00:09:24 +0000

    Completed a few more functions. Almost done

--- binary-serach-trees.js
+++ binary-serach-trees.js
@@ -10,6 +10,7 @@ class Node {
 
 class Tree {
     constructor(array) {
+        // sort the integers into ascending order and remove duplicate integers
         let sortAndCleanArray = array.sort();
         sortAndCleanArray = array.filter((num, index) => {
             return array.indexOf(num) === index;
@@ -17,6 +18,7 @@ class Tree {
         this.root = this.buildTree(sortAndCleanArray, 0, sortAndCleanArray.length - 1);
     }
 
+    // builds the balanced binary search tree
     buildTree(array, start, end) {
         if (start > end) return null;
 
@@ -28,6 +30,7 @@ class Tree {
         return root;
     }
 
+    // inserts a value at the end of one of the tree branches depending on the value
     insert(value, root = this.root) {
         if (root === null) {
             root = new Node(value);
@@ -42,6 +45,7 @@ class Tree {
         return root;
     }
 
+    // deletes a value from the tree
     delete(value, root = this.root) {
         if (root === null) return root;
 
@@ -70,28 +74,111 @@ class Tree {
         return minValue;
     }
 
-    find(value) {
-        let currentNode = this.data;
+    // searches for a value and returns the node if found
+    find(value, root = this.root) {
+        if (root === null) return "Value not found";
+
+        if (value === root.data) return root;
+
+        if (value < root.data) {
+            return root.left = this.find(value, root.left)
+        } else if (value > root.data) {
+            return root.right = this.find(value, root.right)
+        }
+        return root;
     }
 
-    levelOrder() {
+    levelOrder(callback, root = this.root) {
+        if (root === null) return;
 
+        let queue = [root];
+        let levelOrderArray = [];
+
+        while (queue.length > 0) {
+            let node = queue.shift();
+            levelOrderArray.push(node.data);
+
+            if (node.left != null) {
+                queue.push(node.left);
+            }
+            if (node.right != null) {
+                queue.push(node.right);
+            }
+
+            if (typeof callback === 'function') {
+                callback(node)
+            }
+        }
+        return levelOrderArray;
     }
 
-    inorder() {
+    inorder(callback, root = this.root, inorderArray = []) {
+        if (root === null) return;
 
+        if (root.left != null) {
+            this.inorder(callback, root.left, inorderArray);
+        }
+        inorderArray.push(root.data);
+        if (root.right != null) {
+            this.inorder(callback, root.right, inorderArray);
+        }
+
+        if (typeof callback === 'function') {
+            callback(root);
+        }
+        return inorderArray;
     }
 
-    preorder() {
+    preorder(callback, root = this.root, preorderArray = []) {
+        if (root === null) return;
 
+        preorderArray.push(root.data)
+
+        if (root.left != null) {
+            this.preorder(callback, root.left, preorderArray)
+        }
+        if (root.right != null) {
+            this.preorder(callback, root.right, preorderArray)
+        }
+
+        if (typeof callback === 'function') {
+            callback(root)
+        }
+
+        return preorderArray;
     }
 
-    postorder() {
+    postorder(callback, root = this.root, postorderArray = []) {
+        if (root === null) return;
 
+        if (root.left != null) {
+            this.postorder(callback, root.left, postorderArray)
+        }
+        if (root.right != null) {
+            this.postorder(callback, root.right, postorderArray)
+        }
+        postorderArray.push(root.data);
+
+        if (typeof callback === 'function') {
+            callback(root)
+        }
+        return postorderArray;
     }
 
-    height(node) {
+    height(node, root = this.root, count = 1) {
+        if (root === null) return "Not found";
 
+        if (node === root.data) return 1;
+
+        if (node < root.data) {
+            count++
+            return root.left = this.height(node, root.left, count)
+        } else if (node > root.data) {
+            count++
+            return root.right = this.height(node, root.right, count)
+        }
+        console.log(count)
+        console.log(root)
     }
 
     depth() {
@@ -126,3 +213,9 @@ binarySearchTree.insert(50);
 binarySearchTree.insert(25);
 binarySearchTree.delete(50);
 prettyPrint(binarySearchTree.root);
+// console.log(binarySearchTree.find(3))
+binarySearchTree.levelOrder()
+console.log(binarySearchTree.inorder())
+console.log(binarySearchTree.preorder())
+console.log(binarySearchTree.postorder())
+binarySearchTree.height(2);
